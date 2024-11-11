6906216348ce2c73ed272d15598b299a339bfcf2
William Gertler <will.gertler@gmail.com>
2024-11-11 08:33:37 -0500

    add notif functions to timer

--- public/client.js
+++ public/client.js
@@ -5,6 +5,28 @@ const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
 const resetButton = document.getElementById('reset');
 
+// Ask for user permission to send notification
+function requestNotificationPermission() {
+    if (Notification.permission === 'default') {
+        Notification.requestPermission().then((permission) => {
+            if (permission === 'granted') {
+                console.log('Notification permission granted.');
+            }
+        });
+    }
+}
+requestNotificationPermission();
+
+function sendNotification(message, iconpath) {
+    if (Notification.permission === 'granted') {
+        new Notification('Web Timer', {
+            body: message,
+            icon: iconpath 
+        });
+    }
+}
+
+
 // Update the timer display when receiving a new timer value
 socket.on('timer-update', (time) => {
     const minutes = Math.floor(time / 60);
@@ -15,12 +37,15 @@ socket.on('timer-update', (time) => {
 // Editor controls
 startButton.addEventListener('click', () => {
     socket.emit('start-timer');
+    sendNotification('Timer Started', 'img/start.png')
 });
 
 stopButton.addEventListener('click', () => {
     socket.emit('stop-timer');
+    sendNotification('Timer Stopped', 'img/stop.png')
 });
 
 resetButton.addEventListener('click', () => {
     socket.emit('reset-timer');
+    sendNotification('Timer Reset', 'img/reset.png')
 });
